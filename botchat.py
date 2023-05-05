df5db0a04d801b070154dd96e7a9312cf7cda3ab
Andrea Grandi <a.grandi@gmail.com>
2023-05-05 13:25:16 +0200

    Use gpt-4 and pass initial context around

--- botchat.py
+++ botchat.py
@@ -6,45 +6,52 @@ import os
 OPENAI_API_KEY_1 = os.environ.get("OPENAI_API_KEY_1")
 OPENAI_API_KEY_2 = os.environ.get("OPENAI_API_KEY_2")
 CONVERSATION_ITERATIONS = 10
-MAX_TOKENS = 120
+OPEN_AI_MODEL = "gpt-4"
 
 
-def conversation(input_text):
+def conversation(input_text, original_context):
+    if input_text != original_context:
+        message = original_context + " " + input_text
+    else:
+        message = input_text
+
     bot_1_response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model=OPEN_AI_MODEL,
         messages=[
-            {"role": "assistant", "content": input_text}
+            {"role": "assistant", "content": message}
         ],
-        max_tokens=MAX_TOKENS,
         presence_penalty=0.5,
         api_key=OPENAI_API_KEY_1,
     )
 
     print(f"BOT 1: {bot_1_response.choices[0].message.content}\n")
 
+    message = original_context + bot_1_response.choices[0].message.content
+
     bot_2_response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model=OPEN_AI_MODEL,
         messages=[
-            {"role": "user", "content": bot_1_response.choices[0].message.content}
+            {"role": "user", "content": message}
         ],
-        max_tokens=MAX_TOKENS,
         presence_penalty=0.5,
         api_key=OPENAI_API_KEY_2,
     )
 
     print(f"BOT 2: {bot_2_response.choices[0].message.content}\n")
-    
+
     return bot_2_response.choices[0].message.content
 
 
 def main():
     # Start the conversation
     input_text = input("Please enter your message: ")
+    original_input = input_text
+
     for i in range(CONVERSATION_ITERATIONS):
-        input_text = conversation(input_text)
-        # There seems to be a limit of 3 requests/minute so with a delay of 12 seconds
+        input_text = conversation(input_text=input_text, original_context=original_input)
+        # There seems to be a limit of 3 requests/minute so with a delay of 10 seconds
         # each API call from different keys should be allowed.
-        time.sleep(12)
+        time.sleep(10)
 
 
 if __name__ == '__main__':
