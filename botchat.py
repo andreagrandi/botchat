d924cad3008393c7d42dcac379ba122c6c519fc2
Jannes Philipp Mönnighoff <jannes@jmcoding.de>
2023-05-05 23:20:43 +0200

    Let GPT-4 add code comments

--- botchat.py
+++ botchat.py
@@ -1,12 +1,32 @@
+"""
+Botchat
+
+Summary:
+This program simulates a conversation between two AI chatbots using OpenAI's GPT-4 model.
+It prompts the user to provide system messages for both bots and then generates a conversation
+between the two bots. The conversation is printed to the console with colored formatting.
+
+Usage:
+1. Ensure that the required libraries (openai, os, copy, and sys) are installed.
+2. Set the OPENAI_API_KEY environment variable to your OpenAI API key.
+3. Run the script in a Python environment:
+   $ python chatbot_conversation.py
+4. Enter the system messages for both bots when prompted.
+5. The script will generate and display a conversation between the two bots.
+"""
+
+# Import necessary libraries
 import openai
 import os
 import copy
 import sys
 
+# Set up API key and other global variables
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 CONVERSATION_ITERATIONS = 20
 OPEN_AI_MODEL = "gpt-4"
 
+# Define color formats for printing in the console
 format = {
     "green": "\033[32m",
     "cyan": "\033[96m",
@@ -15,6 +35,7 @@ format = {
     "reset": "\033[0m",
 }
 
+# Define a function to simulate a conversation between two bots
 def conversation(bot_1_system_message, bot_2_system_message, messages):
     input = copy.deepcopy(messages)
     role = ""
@@ -22,6 +43,7 @@ def conversation(bot_1_system_message, bot_2_system_message, messages):
     SOFT_MAX_COLS = 80
     col_counter = COL_COUNTER_START
 
+    # Determine the role of the current message (assistant or user)
     if len(messages) == 0 or messages[-1]["role"] == "user":
         role = "assistant"
 
@@ -50,6 +72,7 @@ def conversation(bot_1_system_message, bot_2_system_message, messages):
 
         input.insert(0, {"role": "system", "content": bot_2_system_message})
 
+        # Swap roles in the input message list
         for message in input:
             if message["role"] == "user":
                 message["role"] = "assistant"
@@ -58,6 +81,7 @@ def conversation(bot_1_system_message, bot_2_system_message, messages):
 
     response_content = ""
 
+    # Call the OpenAI API to generate responses for the conversation
     for response in openai.ChatCompletion.create(
         model=OPEN_AI_MODEL,
         messages=input,
@@ -83,8 +107,9 @@ def conversation(bot_1_system_message, bot_2_system_message, messages):
 
     return messages
 
-
+# Define the main function
 def main():
+    # Print header and ASCII art
     print(
         format["yellow"]
         + "\n~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~\n"
@@ -108,6 +133,7 @@ def main():
         + format["reset"]
     )
 
+    # Prompt the user for the system messages for both bots
     print("Please enter system messages for the bots.\n")
     bot_1_system_message = input(
         format["green"] + format["bold"] + "BOT 1" + format["reset"] + ": "
@@ -116,6 +142,7 @@ def main():
         format["cyan"] + format["bold"] + "BOT 2" + format["reset"] + ": "
     )
 
+    # If the second bot's system message is empty, use the first bot's system message
     if bot_2_system_message == "":
         bot_2_system_message = bot_1_system_message
         print("(Using the same system message for both)")
@@ -126,6 +153,7 @@ def main():
         + format["reset"]
     )
 
+    # Initialize the messages list and start the conversation
     messages = []
     for i in range(CONVERSATION_ITERATIONS):
         messages = conversation(bot_1_system_message, bot_2_system_message, messages)
